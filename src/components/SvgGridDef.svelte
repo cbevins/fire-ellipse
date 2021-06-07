b05e4b5d48841be5af13b48ff26604a395c49070
Collin D. Bevins <cbevins@montana.com>
2021-06-07 09:05:58 -0600

    Added SvgGridDef.svelte

---
+++ src/components/SvgGridDef.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let id = 'svg-grid-default'
+  export let rows = 10
+  export let cols = 10
+  export let rowHt = 100
+  export let colWd = 100
+  export let colStyle = "stroke:rgb(127,127,127);stroke-width:2"
+  export let rowStyle = "stroke:rgb(127,127,127);stroke-width:2"
+
+  let xBeg = 0
+  let xEnd = cols * colWd
+  let yBeg = 0
+  let yEnd = rows * rowHt
+  let rowIdx = []
+  let colIdx = []
+  for (let i=0; i<rows; i++) rowIdx.push(i)
+  for (let i=0; i<cols; i++) colIdx.push(i)
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg">
+  <defs>
+    <g id={id}>
+      {#each rowIdx as row}
+        <line x1={xBeg} x2={xEnd}
+          y1={row * rowHt} y2={row * rowHt} style={rowStyle}/>
+      {/each}
+      {#each colIdx as col}
+        <line x1={col * colWd} x2={col * colWd}
+          y1={yBeg} y2={yEnd} style={colStyle}/>
+      {/each}
+    </g>
+  </defs>
+</svg>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,36 @@
 <script>
+  import { writable } from 'svelte/store'
+  import { ignite, grow } from '../models/ellipse.js'
+  import SvgGridDef from '../components/SvgGridDef.svelte'
+
+  // array of fire polygon boundary points
+  let boundary = writable([])
+  // string representation for use with <polyline>
+  let polyline = writable('')
+
+  // Simulation parameters
+  let buffer = 1e-8 // Ellipse.containsPoint() buffer
+  let degrees = 30 // 12 boundary points spawned per ellipse
+  let dur = 1
+  let steps = 0
+  let start = 0
+  let current = start
+
+  function north (a, x, y) { return `rotate(${a - 90}, ${x}, ${y})` }
+
+  function update (bdyPts) {
+    $boundary = bdyPts
+    steps++
+    current = start + steps * dur
+  }
+
+  function step () {
+    update(grow($boundary, current, dur, degrees, buffer))
+  }
+
+  // Ignite the fire ...
+  let [ellipse, bdyPts] = ignite(500, 500, start, dur, degrees)
+  update(bdyPts)
 </script>
 
 <svelte:head>
@@ -6,11 +38,17 @@
 </svelte:head>
 
 <h1>Fire Ellipse Tinker Project</h1>
-<style>
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
+<p>Currently at step {steps} with {$boundary.length} points</p>
+<button on:click={step}>Grow</button>
+
+<svg viewbox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+  <defs>
+    <SvgGridDef id='grid'/>
+  </defs>
+
+  <use xlink:href="#grid" />
+  {#each $boundary as e}
+    <circle cx={e.x} cy={e.y} r="4" stroke="black" stroke-width="1" fill="red" />
+  {/each}
+</svg>
